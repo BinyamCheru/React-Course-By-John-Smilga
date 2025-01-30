78ed1839f36f3578305b87083a3e12db96c92cfe
Bina <benjaminbinyam500@gmail.com>
2025-01-30 11:45:12 +0300

    applying js in JSX

--- src/index.js
+++ src/index.js
@@ -10,35 +10,18 @@ const BookList = () => {
     </section>
   );
 };
-
+const author = "Mel Robbins";
 const Book = () => {
+  const title = "The Let Them Theory";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="./images/book-2.jpg" alt="The Let Them Theory" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
-const Image = () => {
-  return <img src="./images/book-2.jpg" alt="The Let Them Theory" />;
-};
-const Title = () => <h2>The Let Them Theory</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>Mel Robbins</h4>;
-  // return <h4 style={
-  // {
-  //   color: "#617d98",
-  //   fontSize: "0.75rem",
-  //   marginTop: "0.5rem",
-  // }
-  // }>Mel Robbins</h4>;
-};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
