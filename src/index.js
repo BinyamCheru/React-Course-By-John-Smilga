8ca19eea2f98148e2c67536ebf10aa5f365310ef
Bina <benjaminbinyam500@gmail.com>
2025-01-30 10:50:01 +0300

    simple project start creating components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// function Greeting() {
-//   return <h1>My First Component</h1>;
-// }
 
-// we can use an arrow function also
-const Greeting = () => {
+const BookList = () => {
   return (
-    <div>
-      <Heading />
-      <Paragraph />
-    </div>
+    <section>
+      <Book />
+      <Book />;
+      <Book />;
+    </section>
   );
 };
 
-const Heading = () => <h1>This Heading</h1>;
-const Paragraph = () => {
+const Book = () => {
   return (
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, aliquam!</p>
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
   );
 };
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg"
+      alt="The Let Them Theory"
+    />
+  );
+}
+const Title = () => <h2>The Let Them Theory</h2>;
+const Author = () => <h3>Mel Robbins</h3>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
