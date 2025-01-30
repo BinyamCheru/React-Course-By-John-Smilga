23d164b039ae9f07d042941e24773824a5dbdadb
Bina <benjaminbinyam500@gmail.com>
2025-01-30 19:41:02 +0300

    Access Props by Destructuring (object)

--- src/index.js
+++ src/index.js
@@ -41,11 +41,13 @@ const BookList = () => {
 };
 
 const Book = (props) => {
+  // or here simply pass const Book = ({ img, title, author }) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
