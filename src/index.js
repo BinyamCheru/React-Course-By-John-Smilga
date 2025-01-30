6f15e7a6c0fe06c4674503e4218b9ee4754d7793
Bina <benjaminbinyam500@gmail.com>
2025-01-30 19:30:08 +0300

    Props - Somewhat Dynamic Setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-const img = "./images/book-2.jpg";
-const title = "The Let Them Theory";
-const author = "Mel Robbins";
+
+const firstBook = {
+  img: "./images/book-1.jpg",
+  title: "The Let Them Theory",
+  author: "Mel Robbins",
+};
+const secondBook = {
+  img: "./images/book-2.jpg",
+  title: "The Crash",
+  author: "Freida McFadden",
+};
+const thirdBook = {
+  img: "./images/book-3.jpg",
+  title: "Brimstone (Deluxe Limited Edition)",
+  author: "Callie Hart",
+};
 
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book img={img} title={title} author={author} />
-      <Book img={img} title={title} author={author} />
-      <Book img={img} title={title} author={author} />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
     </section>
   );
 };
