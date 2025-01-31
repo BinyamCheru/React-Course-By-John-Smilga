7437c0fd6a439bdc6fc0a2b7bc4ea9ca2942940e
Bina <benjaminbinyam500@gmail.com>
2025-01-31 10:49:38 +0300

    Local Images (src folder) - better performance

--- src/books.js
+++ src/books.js
@@ -1,18 +1,21 @@
+import img1 from "./images/book-1.jpg"
+import img2 from "./images/book-2.jpg"
+import img3 from "./images/book-3.jpg"
 const books = [
   {
-    img: "./images/book-1.jpg",
+    img: img1,
     title: "The Let Them Theory",
     author: "Mel Robbins",
     id: 1,
   },
   {
-    img: "./images/book-2.jpg",
+    img: img2,
     title: "The Crash",
     author: "Freida McFadden",
     id: 2,
   },
   {
-    img: "./images/book-3.jpg",
+    img: img3,
     title: "Brimstone (Deluxe Limited Edition)",
     author: "Callie Hart",
     id: 3,
