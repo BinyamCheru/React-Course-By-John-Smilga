d0f48938761bd6b3d9fce74a0cc1e39b85b5032a
Bina <benjaminbinyam500@gmail.com>
2025-01-31 09:17:32 +0300

    events in react

--- src/index.js
+++ src/index.js
@@ -24,15 +24,48 @@ const books = [
 
 const BookList = () => {
   return (
-    <section className="bookList">
-      {books.map((book) => {
-        // return <Book book={book} key={book.id} />; this is option one without using spread operator
-        return <Book {...book} key={book.id} />;
-      })}
-    </section>
+    <>
+      <EventExample />
+      <section className="bookList">
+        {books.map((book) => {
+          // return <Book book={book} key={book.id} />; this is option one without using spread operator
+          return <Book {...book} key={book.id} />;
+        })}
+      </section>
+    </>
   );
 };
 
+const EventExample = () => {
+  const handleFormInput = (event) => {
+    console.log(event.target);
+    console.log(event.target.name);
+    console.log(event.target.value);
+
+    console.log("Handle Form Input.");
+  };
+  const handleButtonClick = () => {
+    alert("Handle Click Event");
+  };
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+    console.log("Form Submitted.");
+  };
+  return (
+    <section style={{ margin: "2rem" }}>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click Me</button>
+    </section>
+  );
+};
 const Book = (props) => {
   const { img, title, author } = props;
   // const { img, title, author } = props.book; without using spread operator
