34032460ab644f4cd1d6d2a91b37d34b07a36e3e
Bina <benjaminbinyam500@gmail.com>
2025-01-31 09:34:43 +0300

    components are independent by default

--- src/index.js
+++ src/index.js
@@ -75,11 +75,14 @@ const EventExample = () => {
 };
 const Book = (props) => {
   const { img, title, author } = props;
-  // const { img, title, author } = props.book; without using spread operator
+  const displayTitle = () => {
+    console.log(title);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>Display Title</button>
       <h4>{author}</h4>
     </article>
   );
